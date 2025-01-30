854611657fcec0c6049ff67d183a38e69581a938
fburman666 <fburman@gmail.com>
2025-01-30 17:26:13 +0100

    first commit

--- 2_main.py
+++ 2_main.py
@@ -1,38 +1,30 @@
+from functions_modules import *
 
 print("2.1")
-from functions_modules import my_function
 my_function("Fredrik")
 
 print("2.2a")
-from functions_modules import eko
 eko("hej")
 
 print("2.2b")
-from functions_modules import eko2b
 eko2b("hej", 7)
 
 print("2.3")
-from functions_modules import loop_function
 loop_function()
 
 print("2.4")
-from functions_modules import last
 last([1,2,3,4,5,6,7])
 
 print("2.5")
-from functions_modules import cut_edges
 cut_edges([1, 2, 3, 4])
 
 print("2.6")
-from functions_modules import increase
 print(increase(1))
 
 print("2.7")
-from functions_modules import average
 print(average(23,67))
 
 print("2.8")
-from functions_modules import pretty_print
 pretty_print(["a", "b", 3.14])
 pretty_print([])
 
